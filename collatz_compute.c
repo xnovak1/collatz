0ec22a8e8f1a7f681d05178ac45b605c621bcd01
Michal Novák <xnovak9@fi.muni.cz>
2023-11-27 21:07:21 +0100

    afoabf

--- collatz_compute.c
+++ collatz_compute.c
@@ -1,12 +1,17 @@
+#define _POSIX_C_SOURCE  200809L
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <err.h>
 
 /*
    Command line program that computes number of steps in a Collatz function
    for the first n numbers. Result is stored in a .results file in the csv format
    {input,steps}.
 */
+
+// gcc -std=c99 -Werror -Wall collatz_compute.c -o compute && ./compute
 
 int collatz_compute(int n) {
     if (n <= 0)
@@ -33,17 +38,17 @@ int main(int argc, char *argv[]) {
 
     printf("Computation started ...\n");
 
-    int fd = open(FILENAME, O_WRONLY | O_CREAT | O_APPEND);
+    int fd = open(FILENAME, O_WRONLY | O_CREAT);
     if (fd == -1)
         err(1, "%s", FILENAME);
 
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i <= size; i++) {
         int result = collatz_compute(i);
         if (dprintf(fd, "%d,%d\n", i, result) == -1)
             err(1, NULL);
     }
 
-    printf("Task complete.");
+    printf("Task complete.\n");
 
     return 0;
 }
